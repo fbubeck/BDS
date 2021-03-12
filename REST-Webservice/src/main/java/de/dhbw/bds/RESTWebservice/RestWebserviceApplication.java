023c7d74feb5cdb7f6aef05806a0756e28ed9e19
fbubeck <ferdinand.bubeck@gmx.de>
2021-03-12 18:27:12 +0100

    commit

--- REST-Webservice/src/main/java/de/dhbw/bds/RESTWebservice/RestWebserviceApplication.java
+++ REST-Webservice/src/main/java/de/dhbw/bds/RESTWebservice/RestWebserviceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestWebserviceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("starting ...");
 		SpringApplication.run(RestWebserviceApplication.class, args);
 		System.out.println("stopping ...");
 	}
