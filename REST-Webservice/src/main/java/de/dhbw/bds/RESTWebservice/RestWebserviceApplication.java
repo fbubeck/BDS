ae5b98c025d624629b3011f9df28d738923a4386
jo-bubeck <johannes.bubeck@gmx.net>
2021-03-12 18:12:30 +0100

    stopping

--- REST-Webservice/src/main/java/de/dhbw/bds/RESTWebservice/RestWebserviceApplication.java
+++ REST-Webservice/src/main/java/de/dhbw/bds/RESTWebservice/RestWebserviceApplication.java
@@ -8,6 +8,7 @@ public class RestWebserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestWebserviceApplication.class, args);
+		System.out.println("stopping ...");
 	}
 
 }
